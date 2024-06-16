543212a17d3c9824e360b205129bbd45391c0f5d
Shehan Chrishmika <shehanchrishmika123@gmail.com>
2024-06-16 21:20:42 +0530

    Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+var chanceCount = 10;
+
 function getPlayerName() {
-  const userName = document.getElementsByClassName(".userNameEnterField").value;
+  const userName = document.querySelector(".userNameEnterField");
+  console.log(userName);
 }
+
+function playButtonFunction() {
+  const userName = document.querySelector(".userNameEnterField").value;
+  console.log(userName);
+  const user = document.getElementById("showNameOnGame");
+  user.innerHTML = userName;
+
+  const chances = document.getElementById("chancesCountOnGame");
+  chanceCount = 10;
+  chances.innerHTML = chanceCount;
+}
+
+function guessBtnFunction() {
+  console.log("yess");
+  const chances = document.getElementById("chancesCountOnGame");
+  chanceCount--;
+  chances.innerHTML = chanceCount;
+  if (chanceCount <= 0) {
+    const gameDisplay = document.getElementById("displayStatus");
+    gameDisplay.innerHTML = "Game Over!<br /> You Lost";
+  }
+}
+
+// function changeName() {
+//   const user = document.getElementById("showNameOnGame");
+//   user.innerHTML = userName;
+// }
+
+// function goToHowToPlay() {
+//   const howToPlayPage = document.getElementsByClassName("userInfo").display.value;
+//   //   howToPlayPage.style.display = "hidden";
+
+//   //   howToPlayPage.style.color = "red";
+//   console.log("yes");
+//   console.log(howToPlayPage);
+// }
